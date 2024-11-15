bc1f5fb3d18a007011fc1b63b8932d427712605d
otobongfp <otobongfp@gmail.com>
2024-11-15 13:36:14 +0100

    F-2024-4584 - Insecure URL Handling in navigateToWebsite

--- universal-wallet/src/utils/redirectSocialMedia.ts
+++ universal-wallet/src/utils/redirectSocialMedia.ts
@@ -1,45 +1,77 @@
 import {InAppBrowser} from 'react-native-inappbrowser-reborn';
 import {LTO_EXPLORER_URL, LTO_WALLET_URL} from '@env';
+//F-2024-4584 - Insecure URL Handling in navigateToWebsite
+import {sanitizeUrl} from './validateUrl';
 
 const EXPLORER_URL = LTO_EXPLORER_URL || 'https://explorer.testnet.lto.network';
 const WALLET_URL = LTO_WALLET_URL || 'https://wallet.testnet.lto.network';
 
-export const nagivateTo = async (url: string) => {
-  await InAppBrowser.open(url);
+export const navigateTo = async (url: string) => {
+  const sanitized = sanitizeUrl(url);
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  } else {
+    console.error('Invalid URL:', url);
+  }
 };
 
 export const navigateToWebsite = async () => {
-  await InAppBrowser.open('https://ltonetwork.com');
+  const sanitized = sanitizeUrl('https://ltonetwork.com');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
 
 export const navigateToExplorer = async () => {
-  await InAppBrowser.open(EXPLORER_URL);
+  if (EXPLORER_URL) {
+    await InAppBrowser.open(EXPLORER_URL);
+  }
 };
 
 export const navigateToTransaction = async (id: string) => {
-  await InAppBrowser.open(`${EXPLORER_URL}/transactions/${id}`);
+  const transactionUrl = sanitizeUrl(`${EXPLORER_URL}/transactions/${id}`);
+  if (transactionUrl) {
+    await InAppBrowser.open(transactionUrl);
+  }
 };
 
 export const navigateToWebWallet = async () => {
-  await InAppBrowser.open(WALLET_URL);
+  if (WALLET_URL) {
+    await InAppBrowser.open(WALLET_URL);
+  }
 };
 
 export const navigateToTwitter = async () => {
-  await InAppBrowser.open('https://twitter.com/TheLTONetwork');
+  const sanitized = sanitizeUrl('https://twitter.com/TheLTONetwork');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
 
 export const navigateToFacebook = async () => {
-  await InAppBrowser.open('https://facebook.com/TheLTONetwork/');
+  const sanitized = sanitizeUrl('https://facebook.com/TheLTONetwork/');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
 
 export const navigateToTelegram = async () => {
-  await InAppBrowser.open('https://t.me/ltonetwork');
+  const sanitized = sanitizeUrl('https://t.me/ltonetwork');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
 
 export const navigateToLinkedin = async () => {
-  await InAppBrowser.open('https://linkedin.com/company/ltonetwork/');
+  const sanitized = sanitizeUrl('https://linkedin.com/company/ltonetwork/');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
 
 export const navigateToGithub = async () => {
-  await InAppBrowser.open('https://github.com/ltonetwork/');
+  const sanitized = sanitizeUrl('https://github.com/ltonetwork/');
+  if (sanitized) {
+    await InAppBrowser.open(sanitized);
+  }
 };
