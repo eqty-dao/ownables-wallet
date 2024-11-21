7afbdf257c53c906e769e6e951b028382ab90fac
Brad <bht822@mail.usask.ca>
2024-11-21 15:51:53 -0700

    F-2024-4527 - Exported Activity Vulnerability: Switched to a different static server

--- universal-wallet/plugins/web-server/android/src/newarch/RlWebServerSpec.java
+++
@@ -1,9 +0,0 @@
-package com.rlwebserver;
-
-import com.facebook.react.bridge.ReactApplicationContext;
-
-abstract class RlWebServerSpec extends NativeRlWebServerSpec {
-  RlWebServerSpec(ReactApplicationContext context) {
-    super(context);
-  }
-}
