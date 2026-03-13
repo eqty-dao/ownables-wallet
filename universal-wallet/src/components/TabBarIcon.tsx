fc69eca3e5c578cf8b93c0626abac5d32967b201
arnold <arnold@jasny.net>
2026-03-13 16:41:58 +0100

    Remove unused UI components except update modal

--- universal-wallet/src/components/TabBarIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import {IconButton} from 'react-native-paper';
-
-export default function TabBarIcon(props: {
-  icon: React.ComponentProps<typeof IconButton>['icon'];
-  color: string;
-}): JSX.Element {
-  return <IconButton testID="tab-bar-icon" size={35} style={{marginHorizontal: -15, marginVertical: -15}} {...props} />;
-}
--- universal-wallet/src/components/styles/ConfirmationDialog.styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native';
-
-export const ButtonContainer = styled.View`
-  flex-direction: row;
-  justify-content: space-around;
-`;
